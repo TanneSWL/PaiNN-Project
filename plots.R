48f8bb34a1a30832de5c00aa429529d5db921381
jobaov <143402275+jobaov@users.noreply.github.com>
2024-12-20 18:49:54 +0100

    Minor changes

--- plots.R
+++ plots.R
@@ -3,6 +3,7 @@ rm(list = ls())
 library(tidyverse)
 library(latex2exp)
 library(ggrepel)
+library(patchwork)
 
 #-------------------------------------------------------------------------------
 # Number if layers vs. performance
@@ -12,7 +13,7 @@ MAE <- list()
 
 # Extract the results for test MAE for each run. 
 for(i in 1:length(layers)){
-  MAE[i] <- suppressWarnings(readLines(paste("output_", layers[i], "L/test_MAE.csv", sep = "")))
+  MAE[i] <- suppressWarnings(readLines(paste("results/output_", layers[i], "L/test_MAE.csv", sep = "")))
 }
 
 MAE <- as.numeric(MAE)
@@ -48,14 +49,14 @@ ggsave("MAE_layers.png",
 # Worse predictions
 
 # Get predictions for 9 layers.
-data_pred <- read.table("output_9L/predictions_vs_true_labels_pandas.csv",
+data_pred <- read.table("results/output_9L/predictions_vs_true_labels_pandas.csv",
                         sep = ",",
                         header = TRUE)
 
 # Compute MAE.
 data_pred <- tibble(data_pred) |>
   rename(True = "True.Labels") |>
-  mutate(MAE= True - Predictions)
+  mutate(MAE = True - Predictions)
 
 # Extract the worse predictions in a seperate set.
 data_pred_out <- data_pred |>
@@ -72,7 +73,7 @@ ggplot(data = data_pred,
              size = 2,
              alpha = 0.5) +
   xlab("True Internal Energy (0 K) [eV]") + 
-  ylab("MAE [eV]") +
+  ylab("Error [eV]") +
   theme(axis.title.x = element_text(margin = margin(t = 20), size = 14, face = "bold"),
         axis.title.y = element_text(margin = margin(r = 20), size = 14, face = "bold"),
         axis.text.x = element_text(size = 10),
@@ -103,8 +104,9 @@ data_pred <- data_pred |>
          Negative = str_count(Smiles, "-"),
          Total = Positive + Negative,
          Nitrogen = str_count(Smiles, "N"),
-         Atoms = str_count(Smiles, "[A-Z]"),
-         Nitrogen_frac = Nitrogen/Atoms)
+         Atoms = str_count(Smiles, "[A-Z]") - str_count(Smiles, "H"),
+         Nitrogen_frac = Nitrogen/Atoms,
+         absMAE = abs(MAE))
 
 data_pred_out <- data_pred |>
   filter(abs(MAE) > 0.25) |>
@@ -115,57 +117,47 @@ data_pred_out <- data_pred |>
 lookForPatter <- function(feature, xlabel){
   ggplot(data = data_pred,
          mapping = aes(x = {{feature}},
-                       y = MAE)) +
+                       y = absMAE)) +
     geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
     geom_point(color = "#CC6600",
                size = 2,
                alpha = 0.5) +
     xlab( xlabel) + 
-    ylab("MAE [eV]") +
+    ylab("Absolute Error [eV]") +
     theme(axis.title.x = element_text(margin = margin(t = 20), size = 14, face = "bold"),
           axis.title.y = element_text(margin = margin(r = 20), size = 14, face = "bold"),
           axis.text.x = element_text(size = 10),
-          axis.text.y = element_text(size = 10)) + 
-    geom_point(data = data_pred_out,
-               mapping = aes(x = {{feature}},
-                             y = MAE),
-               color = "#030F4F",
-               size = 4,
-               shape = 21) + 
-    geom_text_repel(data = data_pred_out,
-                    mapping = aes(x = {{feature}},
-                                  y = MAE,
-                                  label = Number))
+          axis.text.y = element_text(size = 10))
 }
 
 
-lookForPatter(Positive, "Number of Positive Charges")
-ggsave("positive.png",
+p1 <- lookForPatter(Positive, "Number of Positive Charges")
+
+p2 <- lookForPatter(Negative, "Number of Negative Charges") +
+  scale_x_continuous(limits = c(0, 2), breaks = seq(0, 2, by = 1))
+
+p3 <- lookForPatter(Total, "Total Number of Charges")
+
+(p1 / p2 / p3) + plot_annotation(tag_levels = 'A')
+
+ggsave("charges.png",
        width = 8,
-       height = 4)
+       height = 12)
 
+p4 <- lookForPatter(Nitrogen, "Number of Nitrogen Atoms") + 
+  scale_x_continuous(limits = c(0, 7), breaks = seq(0, 7, by = 1))
 
-lookForPatter(Negative, "Number of Negative Charges")
-ggsave("negative.png",
-       width = 8,
-       height = 4)
+p5 <- lookForPatter(Atoms, "Number of Heavy Atoms") +
+  scale_x_continuous(limits = c(0, 9), breaks = seq(0, 9, by = 1))
 
-lookForPatter(Total, "Total Number of Charges")
-ggsave("total.png",
-       width = 8,
-       height = 4)
+p6 <- lookForPatter(Nitrogen_frac, "Number of Nitrogen / Number of Atoms")
 
-lookForPatter(Nitrogen, "Number of Nitrogen Atoms")
+(p4 / p5 / p6) + plot_annotation(tag_levels = 'A')
+
 ggsave("nitrogen.png",
        width = 8,
-       height = 4)
+       height = 12)
 
-lookForPatter(Atoms, "Number of Atoms")
-ggsave("atoms.png",
-       width = 8,
-       height = 4)
-
-lookForPatter(Nitrogen_frac, "Number of Nitrogen / Number of Atoms")
-ggsave("nitrogen_frac.png",
-       width = 8,
-       height = 4)
+data_pred |>
+  filter(Atoms == 9) |>
+  count()
